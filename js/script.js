718abb53759bdf2730c976904357c0decad3a900
sultonakobirov <zafarzhanovich@gmail.com>
2023-04-01 06:37:37 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,26 @@
+//task 
+
+let week =["Понидельник","вторник","среда","четверг","пятница","суббота"]
+let date = new Date()
+let day = date.getDay()
+
+//1
+currentDay = week[day - 1]
+console.log(currentDay);
+
+//2
+switch (day) {
+    case 1 || 2 || 3 || 4 || 5 || 6:
+        console.log(week[day - 1]);
+        break;    
+    default:
+        console.log(week[day - 1]);
+        break;
+}
+
+//3
+if(day === 1 || 2 || 3 || 4 || 5 || 6){
+    console.log(week[day - 1]);
+}else{
+    console.log(week[day - 1]);
+}
